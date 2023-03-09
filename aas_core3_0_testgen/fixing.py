56f69c450b4bfbc15898c4b67e39726690f568a8
Marko Ristin <marko.ristin@gmail.com>
2023-03-09 12:18:14 +0100

    wip

---
+++ aas_core3_0_testgen/fixing.py
@@ -0,0 +1,19 @@
+"""
+Fix instances in place to conform to the meta-model constraints.
+
+.. note::
+
+    We fix instances on the best-effort basis. It might be that something goes wrong.
+    Please always verify the results.
+"""
+# TODO (mristin, 2023-03-9): continue here
+
+# TODO (mristin, 2023-03-9): implement Handyman 🠒 use PassThrough visitor
+# TODO (mristin, 2023-03-9): implement dereference function in common
+# TODO (mristin, 2023-03-9): implement function to assert that the instance still exists in the wrapped environment after fixing
+# TODO (mristin, 2023-03-9):  assert_instance_at_path_in_environment(environment, instance, path)
+
+# TODO (mristin, 2023-03-9): fix(instance: Class)
+
+# TODO (mristin, 2023-03-9): fix_wrapping(environment, instance, path)
+# TODO (mristin, 2023-03-9):  🠒 call assert_instance_at_path_in_environment
